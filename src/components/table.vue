480a966325ecadb59c4c0c6ecea051e8b5458c57
weiqiang165260 <mushangwq@163.com>
2017-08-25 09:27:59 +0800

    自定义组件demo

---
+++ src/components/table.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="table">
+    <div class="row">{{msg}}</div>
+    <div class="row">{{msg}}</div>
+    <div class="row">{{msg | showCase}}</div>
+    <div v-on:click="check">test</div>
+    <span>{{now}}</span>
+  </div>
+
+</template>
+
+<script>
+  /* eslint-disable key-spacing,no-trailing-spaces */
+  export default {
+    name:'world',
+    data () {
+      return {
+        msg :'i am weiqiang'
+      }
+    },
+    methods:{
+      check:function (event) {
+        alert(event.target.tagName)
+      }
+    },
+    computed:{
+      now :function () {
+        return new Date()
+      }
+    },
+    filters:{
+      showCase :function (value) {
+        if (!value) return
+        value = value.toString()
+        return value.toUpperCase()
+      }
+    }
+
+  }
+</script>
+<style scoped>
+  .table{
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    flex: 1;
+  }
+  .table .row{
+    padding: 0 10px;
+  }
+</style>
